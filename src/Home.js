4b2cfe1702fe79c85b4d764ce7809cf672e088e8
Aayushi Sah <88141650+aayushisah@users.noreply.github.com>
2023-07-15 04:33:42 +0530

    Added BlogList prop

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return (
-      <div className="home">
-        <h2>Homepage</h2>
-      </div>
-    );
-  }
-   
-  export default Home;
+  const [blogs, setBlogs] = useState([
+    { title: 'Hello', body: 'lorem ipsum...', author: 'aayushi', id: 1 },
+    { title: 'Coming soon ...', body: 'lorem ipsum...', author: 'aayushi', id: 2 },
+    { title: 'Coming soon ...', body: 'lorem ipsum...', author: 'aayushi', id: 3 }
+  ])
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" />
+    </div>
+  );
+}
+ 
+export default Home;
