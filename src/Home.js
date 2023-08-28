ab944b86928b7df5cc7d73afab425d6022b8eeeb
Aayushi Sah <88141650+aayushisah@users.noreply.github.com>
2023-08-28 22:34:48 +0530

    Added json server

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'Hello', body: 'lorem ipsum...', author: 'aayushi', id: 1 },
-    { title: 'Coming soon ...', body: 'lorem ipsum...', author: 'aayushi', id: 2 },
-    { title: 'Coming soon ...', body: 'lorem ipsum...', author: 'aayushi', id: 3 }
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+
+  const [name, setName] = useState('mario');
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  }, [name])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
       <nav className="navbar">
-        <h1>stuff from here, there and everywhere</h1>
+        <h1>(aa)yu go for it!</h1>
         <div className="links">
           <a href="/">Home</a>
           <a href="/create" style={{ 
